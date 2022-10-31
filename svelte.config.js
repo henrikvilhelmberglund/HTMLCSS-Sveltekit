f468e783ba0ea3774653a4bf4737461b4187af11
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-10-31 19:19:53 +0100

    add static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: true
+    }),
+
     paths: {
       base: dev ? "" : "/HTMLCSS-Sveltekit/",
     }
-  }
+  },
 };
 
 export default config;
