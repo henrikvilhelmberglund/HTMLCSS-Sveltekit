b6283845b2be830b77d2dec0be9b3bea6b03a40c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-10-31 19:16:59 +0100

    change base location to svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
 import adapter from '@sveltejs/adapter-auto';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: dev ? "" : "/HTMLCSS-Sveltekit/",
+    }
+  }
 };
 
 export default config;
