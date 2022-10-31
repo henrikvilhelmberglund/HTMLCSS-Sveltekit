23fdacf83b4230843109ddec1f6cd9da065bc719
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-10-31 20:14:04 +0100

    Updates

---
+++ _app/immutable/components/pages/_page.svelte-0cf42796.js
@@ -0,0 +1,129 @@
+import{S as Ct,i as Mt,s as Rt,k as a,q as s,a as _,l as o,m as f,r,h as t,c as m,n as v,I as Kl,b as i,G as l,B as Fe,J as Wl,w as gt,x as el,y as tl,f as ll,t as il,z as sl}from"../../chunks/index-21032912.js";import{b as mt}from"../../chunks/paths-6cd3a76e.js";function Ql(je){let d,c,P,E,A,w,N,L,q,I,x,b,R,k,p,u,h,D,y,g,J,pe,C,_e,M,me,S,He,he,U,$e,de,z,Pe,Te,O,F,j,ne,Ae,ee,te,we,le,ie,Ne,se,ce,Ee,V,Ge,be,G,Ce,ke,H,fe;return{c(){d=a("nav"),c=a("ul"),P=a("li"),E=a("a"),A=s("Hem"),w=_(),N=a("li"),L=a("a"),q=s("Om oss"),I=_(),x=a("li"),b=a("a"),R=s("Kontakta oss"),k=_(),p=a("div"),u=a("select"),h=a("option"),D=s("V\xE4lj spr\xE5k"),y=a("option"),g=s("Svenska"),J=a("option"),pe=s("Engelska"),C=a("option"),_e=s("Spanska"),M=a("option"),me=s("Japanska"),S=a("option"),He=s("Japanska"),he=_(),U=a("h1"),$e=s("HTML - Introduktion"),de=_(),z=a("h2"),Pe=s("Javascript \xE4r kul!"),Te=_(),O=a("a"),F=a("div"),j=a("img"),Ae=_(),ee=a("p"),te=s("Click!"),we=_(),le=a("p"),ie=s(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident amet\r
+    repellendus temporibus, eos expedita aperiam asperiores perspiciatis velit\r
+    molestias debitis placeat consequuntur magni sequi explicabo. Quia, ipsa!\r
+    Velit, magnam vel?`),Ne=_(),se=a("h3"),ce=s("F\xF6rdelarna med JS"),Ee=_(),V=a("p"),Ge=s("Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, quisquam?"),be=_(),G=a("h2"),Ce=s("Men CSS \xE4r roligare!"),ke=_(),H=a("p"),fe=s("Lorem ipsum dolor sit amet."),this.h()},l($){d=o($,"NAV",{});var re=f(d);c=o(re,"UL",{});var K=f(c);P=o(K,"LI",{});var Ke=f(P);E=o(Ke,"A",{href:!0,class:!0});var Me=f(E);A=r(Me,"Hem"),Me.forEach(t),Ke.forEach(t),w=m(K),N=o(K,"LI",{});var Q=f(N);L=o(Q,"A",{href:!0,class:!0});var Y=f(L);q=r(Y,"Om oss"),Y.forEach(t),Q.forEach(t),I=m(K),x=o(K,"LI",{});var We=f(x);b=o(We,"A",{href:!0,class:!0});var Re=f(b);R=r(Re,"Kontakta oss"),Re.forEach(t),We.forEach(t),K.forEach(t),re.forEach(t),k=m($),p=o($,"DIV",{class:!0});var T=f(p);u=o(T,"SELECT",{name:!0,id:!0});var X=f(u);h=o(X,"OPTION",{});var Ve=f(h);D=r(Ve,"V\xE4lj spr\xE5k"),Ve.forEach(t),y=o(X,"OPTION",{});var W=f(y);g=r(W,"Svenska"),W.forEach(t),J=o(X,"OPTION",{});var Le=f(J);pe=r(Le,"Engelska"),Le.forEach(t),C=o(X,"OPTION",{});var Qe=f(C);_e=r(Qe,"Spanska"),Qe.forEach(t),M=o(X,"OPTION",{});var Ye=f(M);me=r(Ye,"Japanska"),Ye.forEach(t),S=o(X,"OPTION",{});var xe=f(S);He=r(xe,"Japanska"),xe.forEach(t),X.forEach(t),he=m(T),U=o(T,"H1",{});var ue=f(U);$e=r(ue,"HTML - Introduktion"),ue.forEach(t),de=m(T),z=o(T,"H2",{});var Xe=f(z);Pe=r(Xe,"Javascript \xE4r kul!"),Xe.forEach(t),Te=m(T),O=o(T,"A",{href:!0,target:!0,class:!0});var Be=f(O);F=o(Be,"DIV",{class:!0});var B=f(F);j=o(B,"IMG",{src:!0,width:!0,alt:!0}),Ae=m(B),ee=o(B,"P",{});var Ie=f(ee);te=r(Ie,"Click!"),Ie.forEach(t),B.forEach(t),Be.forEach(t),we=m(T),le=o(T,"P",{});var Ze=f(le);ie=r(Ze,`Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident amet\r
+    repellendus temporibus, eos expedita aperiam asperiores perspiciatis velit\r
+    molestias debitis placeat consequuntur magni sequi explicabo. Quia, ipsa!\r
+    Velit, magnam vel?`),Ze.forEach(t),Ne=m(T),se=o(T,"H3",{});var ge=f(se);ce=r(ge,"F\xF6rdelarna med JS"),ge.forEach(t),Ee=m(T),V=o(T,"P",{});var ye=f(V);Ge=r(ye,"Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, quisquam?"),ye.forEach(t),be=m(T),G=o(T,"H2",{});var et=f(G);Ce=r(et,"Men CSS \xE4r roligare!"),et.forEach(t),ke=m(T),H=o(T,"P",{});var qe=f(H);fe=r(qe,"Lorem ipsum dolor sit amet."),qe.forEach(t),T.forEach(t),this.h()},h(){v(E,"href",mt+"/"),v(E,"class","svelte-1r7n78z"),v(L,"href",mt+"/about"),v(L,"class","svelte-1r7n78z"),v(b,"href",mt+"/contact"),v(b,"class","svelte-1r7n78z"),h.disabled=!0,h.hidden=!0,h.selected=!0,h.__value="V\xE4lj spr\xE5k",h.value=h.__value,y.__value="",y.value=y.__value,J.__value="",J.value=J.__value,C.__value="",C.value=C.__value,C.disabled=!0,M.__value="",M.value=M.__value,M.hidden=!0,S.__value="",S.value=S.__value,v(u,"name",""),v(u,"id",""),Kl(j.src,ne=mt+"/vite.svg")||v(j,"src",ne),v(j,"width","100px"),v(j,"alt","Vite Logo"),v(F,"class","svelte-1r7n78z"),v(O,"href","https://vitejs.dev"),v(O,"target","_blank"),v(O,"class","svelte-1r7n78z"),v(p,"class","svelte-1r7n78z")},m($,re){i($,d,re),l(d,c),l(c,P),l(P,E),l(E,A),l(c,w),l(c,N),l(N,L),l(L,q),l(c,I),l(c,x),l(x,b),l(b,R),i($,k,re),i($,p,re),l(p,u),l(u,h),l(h,D),l(u,y),l(y,g),l(u,J),l(J,pe),l(u,C),l(C,_e),l(u,M),l(M,me),l(u,S),l(S,He),l(p,he),l(p,U),l(U,$e),l(p,de),l(p,z),l(z,Pe),l(p,Te),l(p,O),l(O,F),l(F,j),l(F,Ae),l(F,ee),l(ee,te),l(p,we),l(p,le),l(le,ie),l(p,Ne),l(p,se),l(se,ce),l(p,Ee),l(p,V),l(V,Ge),l(p,be),l(p,G),l(G,Ce),l(p,ke),l(p,H),l(H,fe)},p:Fe,i:Fe,o:Fe,d($){$&&t(d),$&&t(k),$&&t(p)}}}class Yl extends Ct{constructor(d){super(),Mt(this,d,null,Ql,Rt,{})}}function Xl(je){let d,c,P,E,A,w,N,L,q,I,x,b,R,k,p,u,h,D,y,g,J,pe,C,_e,M,me,S,He,he,U,$e,de,z,Pe,Te,O,F,j,ne,Ae,ee,te,we,le,ie,Ne,se,ce,Ee,V,Ge,be,G,Ce,ke,H,fe,$,re,K,Ke,Me,Q,Y,We,Re,T,X,Ve,W,Le,Qe,Ye,xe,ue,Xe,Be,B,Ie,Ze,ge,ye,et,qe,ae,ht,De,Vt,dt,ve,tt,Bt,pt,qt,lt,Dt,Tt,oe,Z,Jt,Se,Ut,Oe,zt,ct,it,Ft,Et,st,jt,bt,rt,Gt,kt,at,Kt,Lt,ot,Wt,xt,nt,Qt,It,ft,Yt,yt,ut,St,Je,Ot,Ue,Ht,ze,_t,vt,Xt,Zt,rl;return{c(){d=s("Which HTML element defines.. a hyperlink = "),c=a("a"),P=s("a link to google"),E=s(`\r
+\r
+an article =\r
+`),A=a("article"),w=s("This is an article tag"),N=s(`\r
+\r
+content aside from the page content =\r
+`),L=a("aside"),q=s("This is content aside from the page content"),I=s(`\r
+\r
+bold text =`),x=a("b"),b=s("This text is bold"),R=s(`\r
+\r
+the document's body = `),k=a("body"),p=s("This is the document body"),u=s(`\r
+\r
+a list item =\r
+`),h=a("li"),D=s("This is a list item"),y=s(`\r
+\r
+the document\u2019s head =`),g=a("head"),J=s("This is the document head"),pe=s(`\r
+\r
+a single line break = `),C=a("br"),_e=s(`\r
+also\r
+`),M=a("hr"),me=s(`\r
+\r
+a clickable button =`),S=a("button"),He=s("Click me"),he=s(`\r
+\r
+a section in a document (OBS! Finns fler \xE4n ett r\xE4tt svar.) =\r
+`),U=a("div"),$e=s("This is a div"),de=_(),z=a("span"),Pe=s("This is a span"),Te=_(),O=a("section"),F=s("This is a section"),j=_(),ne=a("header"),Ae=s("This is a header"),ee=_(),te=a("footer"),we=s("This is a footer"),le=_(),ie=a("nav"),Ne=s("This is a navigation link"),se=s(`\r
+and all other semantic tags\r
+`),ce=a("br"),Ee=s(`\r
+an image =`),V=a("img"),be=s(`\r
+\r
+the main content of a document =\r
+`),G=a("main"),Ce=s("This is the main content of a document"),ke=s(`\r
+\r
+an ordered list =\r
+`),H=a("ol"),fe=a("li"),$=s("This is"),re=_(),K=a("li"),Ke=s("an ordered list"),Me=s(`\r
+\r
+an option in a drop-down list =`),Q=a("select"),Y=a("option"),We=s("This is an option"),Re=s(`\r
+\r
+important text in a paragrapha= `),T=a("strong"),X=s("This text is important"),Ve=s(`\r
+\r
+a row in a table =\r
+`),W=a("table"),Le=a("tr"),Qe=s("This is a row in a table"),Ye=_(),xe=a("tr"),ue=a("td"),Xe=s("This is a cell"),Be=s(`\r
+\r
+an unordered list =\r
+`),B=a("ul"),Ie=a("li"),Ze=s("This is an"),ge=_(),ye=a("li"),et=s("Unordered list"),qe=s(`\r
+\r
+a multiline input control (text area) =`),ae=a("textarea"),ht=s(`\r
+\r
+a table =\r
+`),De=a("table"),Vt=s("This is a table"),dt=_(),ve=a("table"),tt=a("th"),Bt=s("This is a table header"),pt=a("tr"),qt=s("Row"),lt=a("td"),Dt=s("Cell"),Tt=s(`\r
+\r
+a drop-down list =`),oe=a("select"),Z=a("option"),Jt=s("This is a dropdown list"),Se=a("option"),Ut=s("option 1"),Oe=a("option"),zt=s("option 2"),ct=s(`\r
+\r
+a header for a document or section =\r
+`),it=a("header"),Ft=s("This is a header"),Et=s(`\r
+\r
+a heading for a document or section =\r
+`),st=a("h1"),jt=s("This is a heading"),bt=_(),rt=a("h2"),Gt=s("This is a heading used after h1"),kt=_(),at=a("h3"),Kt=s("This is a heading used after h2"),Lt=_(),ot=a("h4"),Wt=s("This is a heading used after h3"),xt=_(),nt=a("h5"),Qt=s("This is a heading used after h4"),It=_(),ft=a("h6"),Yt=s("This is a heading used after h5"),yt=s(`\r
+\r
+an input control =`),ut=a("input"),St=_(),Je=a("input"),Ot=_(),Ue=a("input"),Ht=s(`\r
+\r
+a cell in a table =\r
+`),ze=a("table"),_t=a("tr"),vt=a("td"),Xt=s("This is a cell in a table"),this.h()},l(e){d=r(e,"Which HTML element defines.. a hyperlink = "),c=o(e,"A",{href:!0});var n=f(c);P=r(n,"a link to google"),n.forEach(t),E=r(e,`\r
+\r
+an article =\r
+`),A=o(e,"ARTICLE",{});var al=f(A);w=r(al,"This is an article tag"),al.forEach(t),N=r(e,`\r
+\r
+content aside from the page content =\r
+`),L=o(e,"ASIDE",{});var ol=f(L);q=r(ol,"This is content aside from the page content"),ol.forEach(t),I=r(e,`\r
+\r
+bold text =`),x=o(e,"B",{});var nl=f(x);b=r(nl,"This text is bold"),nl.forEach(t),R=r(e,`\r
+\r
+the document's body = `),k=o(e,"BODY",{});var fl=f(k);p=r(fl,"This is the document body"),fl.forEach(t),u=r(e,`\r
+\r
+a list item =\r
+`),h=o(e,"LI",{});var ul=f(h);D=r(ul,"This is a list item"),ul.forEach(t),y=r(e,`\r
+\r
+the document\u2019s head =`),g=o(e,"HEAD",{});var vl=f(g);J=r(vl,"This is the document head"),vl.forEach(t),pe=r(e,`\r
+\r
+a single line break = `),C=o(e,"BR",{}),_e=r(e,`\r
+also\r
+`),M=o(e,"HR",{}),me=r(e,`\r
+\r
+a clickable button =`),S=o(e,"BUTTON",{});var pl=f(S);He=r(pl,"Click me"),pl.forEach(t),he=r(e,`\r
+\r
+a section in a document (OBS! Finns fler \xE4n ett r\xE4tt svar.) =\r
+`),U=o(e,"DIV",{});var _l=f(U);$e=r(_l,"This is a div"),_l.forEach(t),de=m(e),z=o(e,"SPAN",{});var ml=f(z);Pe=r(ml,"This is a span"),ml.forEach(t),Te=m(e),O=o(e,"SECTION",{});var hl=f(O);F=r(hl,"This is a section"),hl.forEach(t),j=m(e),ne=o(e,"HEADER",{});var dl=f(ne);Ae=r(dl,"This is a header"),dl.forEach(t),ee=m(e),te=o(e,"FOOTER",{});var Tl=f(te);we=r(Tl,"This is a footer"),Tl.forEach(t),le=m(e),ie=o(e,"NAV",{});var cl=f(ie);Ne=r(cl,"This is a navigation link"),cl.forEach(t),se=r(e,`\r
+and all other semantic tags\r
+`),ce=o(e,"BR",{}),Ee=r(e,`\r
+an image =`),V=o(e,"IMG",{src:!0,alt:!0}),be=r(e,`\r
+\r
+the main content of a document =\r
+`),G=o(e,"MAIN",{});var El=f(G);Ce=r(El,"This is the main content of a document"),El.forEach(t),ke=r(e,`\r
+\r
+an ordered list =\r
+`),H=o(e,"OL",{type:!0});var $t=f(H);fe=o($t,"LI",{});var bl=f(fe);$=r(bl,"This is"),bl.forEach(t),re=m($t),K=o($t,"LI",{});var kl=f(K);Ke=r(kl,"an ordered list"),kl.forEach(t),$t.forEach(t),Me=r(e,`\r
+\r
+an option in a drop-down list =`),Q=o(e,"SELECT",{name:!0,id:!0});var Ll=f(Q);Y=o(Ll,"OPTION",{});var xl=f(Y);We=r(xl,"This is an option"),xl.forEach(t),Ll.forEach(t),Re=r(e,`\r
+\r
+important text in a paragrapha= `),T=o(e,"STRONG",{});var Il=f(T);X=r(Il,"This text is important"),Il.forEach(t),Ve=r(e,`\r
+\r
+a row in a table =\r
+`),W=o(e,"TABLE",{class:!0});var Pt=f(W);Le=o(Pt,"TR",{});var yl=f(Le);Qe=r(yl,"This is a row in a table"),yl.forEach(t),Ye=m(Pt),xe=o(Pt,"TR",{});var Sl=f(xe);ue=o(Sl,"TD",{class:!0});var Ol=f(ue);Xe=r(Ol,"This is a cell"),Ol.forEach(t),Sl.forEach(t),Pt.forEach(t),Be=r(e,`\r
+\r
+an unordered list =\r
+`),B=o(e,"UL",{});var At=f(B);Ie=o(At,"LI",{});var Hl=f(Ie);Ze=r(Hl,"This is an"),Hl.forEach(t),ge=m(At),ye=o(At,"LI",{});var $l=f(ye);et=r($l,"Unordered list"),$l.forEach(t),At.forEach(t),qe=r(e,`\r
+\r
+a multiline input control (text area) =`),ae=o(e,"TEXTAREA",{name:!0,id:!0,cols:!0,rows:!0}),f(ae).forEach(t),ht=r(e,`\r
+\r
+a table =\r
+`),De=o(e,"TABLE",{class:!0});var Pl=f(De);Vt=r(Pl,"This is a table"),Pl.forEach(t),dt=m(e),ve=o(e,"TABLE",{class:!0});var wt=f(ve);tt=o(wt,"TH",{class:!0});var Al=f(tt);Bt=r(Al,"This is a table header"),Al.forEach(t),pt=o(wt,"TR",{});var wl=f(pt);qt=r(wl,"Row"),wl.forEach(t),lt=o(wt,"TD",{class:!0});var Nl=f(lt);Dt=r(Nl,"Cell"),Nl.forEach(t),wt.forEach(t),Tt=r(e,`\r
+\r
+a drop-down list =`),oe=o(e,"SELECT",{name:!0,id:!0});var Nt=f(oe);Z=o(Nt,"OPTION",{});var Cl=f(Z);Jt=r(Cl,"This is a dropdown list"),Cl.forEach(t),Se=o(Nt,"OPTION",{});var Ml=f(Se);Ut=r(Ml,"option 1"),Ml.forEach(t),Oe=o(Nt,"OPTION",{});var Rl=f(Oe);zt=r(Rl,"option 2"),Rl.forEach(t),Nt.forEach(t),ct=r(e,`\r
+\r
+a header for a document or section =\r
+`),it=o(e,"HEADER",{});var Vl=f(it);Ft=r(Vl,"This is a header"),Vl.forEach(t),Et=r(e,`\r
+\r
+a heading for a document or section =\r
+`),st=o(e,"H1",{});var Bl=f(st);jt=r(Bl,"This is a heading"),Bl.forEach(t),bt=m(e),rt=o(e,"H2",{});var ql=f(rt);Gt=r(ql,"This is a heading used after h1"),ql.forEach(t),kt=m(e),at=o(e,"H3",{});var Dl=f(at);Kt=r(Dl,"This is a heading used after h2"),Dl.forEach(t),Lt=m(e),ot=o(e,"H4",{});var Jl=f(ot);Wt=r(Jl,"This is a heading used after h3"),Jl.forEach(t),xt=m(e),nt=o(e,"H5",{});var Ul=f(nt);Qt=r(Ul,"This is a heading used after h4"),Ul.forEach(t),It=m(e),ft=o(e,"H6",{});var zl=f(ft);Yt=r(zl,"This is a heading used after h5"),zl.forEach(t),yt=r(e,`\r
+\r
+an input control =`),ut=o(e,"INPUT",{type:!0}),St=m(e),Je=o(e,"INPUT",{type:!0,name:!0,id:!0}),Ot=m(e),Ue=o(e,"INPUT",{type:!0,name:!0,id:!0}),Ht=r(e,`\r
+\r
+a cell in a table =\r
+`),ze=o(e,"TABLE",{class:!0});var Fl=f(ze);_t=o(Fl,"TR",{});var jl=f(_t);vt=o(jl,"TD",{class:!0});var Gl=f(vt);Xt=r(Gl,"This is a cell in a table"),Gl.forEach(t),jl.forEach(t),Fl.forEach(t),this.h()},h(){v(c,"href","http://google.com"),Kl(V.src,Ge=mt+"/svelte.svg")||v(V,"src",Ge),v(V,"alt","svelte logo"),v(H,"type","I"),Y.__value="",Y.value=Y.__value,v(Q,"name",""),v(Q,"id",""),v(ue,"class","svelte-ayl5u2"),v(W,"class","svelte-ayl5u2"),v(ae,"name",""),v(ae,"id",""),v(ae,"cols","30"),v(ae,"rows","10"),ae.value="This is a text area",v(De,"class","svelte-ayl5u2"),v(tt,"class","svelte-ayl5u2"),v(lt,"class","svelte-ayl5u2"),v(ve,"class","svelte-ayl5u2"),Z.__value="",Z.value=Z.__value,Z.disabled=!0,Z.hidden=!0,Z.selected=!0,Se.__value="",Se.value=Se.__value,Oe.__value="",Oe.value=Oe.__value,v(oe,"name",""),v(oe,"id",""),v(ut,"type","button"),ut.value="This is a button input control",v(Je,"type","color"),v(Je,"name","This is a color input control"),v(Je,"id",""),v(Ue,"type","file"),v(Ue,"name","This is a file input control"),v(Ue,"id",""),v(vt,"class","svelte-ayl5u2"),v(ze,"class","svelte-ayl5u2")},m(e,n){i(e,d,n),i(e,c,n),l(c,P),i(e,E,n),i(e,A,n),l(A,w),i(e,N,n),i(e,L,n),l(L,q),i(e,I,n),i(e,x,n),l(x,b),i(e,R,n),i(e,k,n),l(k,p),i(e,u,n),i(e,h,n),l(h,D),i(e,y,n),i(e,g,n),l(g,J),i(e,pe,n),i(e,C,n),i(e,_e,n),i(e,M,n),i(e,me,n),i(e,S,n),l(S,He),i(e,he,n),i(e,U,n),l(U,$e),i(e,de,n),i(e,z,n),l(z,Pe),i(e,Te,n),i(e,O,n),l(O,F),i(e,j,n),i(e,ne,n),l(ne,Ae),i(e,ee,n),i(e,te,n),l(te,we),i(e,le,n),i(e,ie,n),l(ie,Ne),i(e,se,n),i(e,ce,n),i(e,Ee,n),i(e,V,n),i(e,be,n),i(e,G,n),l(G,Ce),i(e,ke,n),i(e,H,n),l(H,fe),l(fe,$),l(H,re),l(H,K),l(K,Ke),i(e,Me,n),i(e,Q,n),l(Q,Y),l(Y,We),i(e,Re,n),i(e,T,n),l(T,X),i(e,Ve,n),i(e,W,n),l(W,Le),l(Le,Qe),l(W,Ye),l(W,xe),l(xe,ue),l(ue,Xe),i(e,Be,n),i(e,B,n),l(B,Ie),l(Ie,Ze),l(B,ge),l(B,ye),l(ye,et),i(e,qe,n),i(e,ae,n),i(e,ht,n),i(e,De,n),l(De,Vt),i(e,dt,n),i(e,ve,n),l(ve,tt),l(tt,Bt),l(ve,pt),l(pt,qt),l(ve,lt),l(lt,Dt),i(e,Tt,n),i(e,oe,n),l(oe,Z),l(Z,Jt),l(oe,Se),l(Se,Ut),l(oe,Oe),l(Oe,zt),i(e,ct,n),i(e,it,n),l(it,Ft),i(e,Et,n),i(e,st,n),l(st,jt),i(e,bt,n),i(e,rt,n),l(rt,Gt),i(e,kt,n),i(e,at,n),l(at,Kt),i(e,Lt,n),i(e,ot,n),l(ot,Wt),i(e,xt,n),i(e,nt,n),l(nt,Qt),i(e,It,n),i(e,ft,n),l(ft,Yt),i(e,yt,n),i(e,ut,n),i(e,St,n),i(e,Je,n),i(e,Ot,n),i(e,Ue,n),i(e,Ht,n),i(e,ze,n),l(ze,_t),l(_t,vt),l(vt,Xt),Zt||(rl=Wl(S,"click",Zl),Zt=!0)},p:Fe,i:Fe,o:Fe,d(e){e&&t(d),e&&t(c),e&&t(E),e&&t(A),e&&t(N),e&&t(L),e&&t(I),e&&t(x),e&&t(R),e&&t(k),e&&t(u),e&&t(h),e&&t(y),e&&t(g),e&&t(pe),e&&t(C),e&&t(_e),e&&t(M),e&&t(me),e&&t(S),e&&t(he),e&&t(U),e&&t(de),e&&t(z),e&&t(Te),e&&t(O),e&&t(j),e&&t(ne),e&&t(ee),e&&t(te),e&&t(le),e&&t(ie),e&&t(se),e&&t(ce),e&&t(Ee),e&&t(V),e&&t(be),e&&t(G),e&&t(ke),e&&t(H),e&&t(Me),e&&t(Q),e&&t(Re),e&&t(T),e&&t(Ve),e&&t(W),e&&t(Be),e&&t(B),e&&t(qe),e&&t(ae),e&&t(ht),e&&t(De),e&&t(dt),e&&t(ve),e&&t(Tt),e&&t(oe),e&&t(ct),e&&t(it),e&&t(Et),e&&t(st),e&&t(bt),e&&t(rt),e&&t(kt),e&&t(at),e&&t(Lt),e&&t(ot),e&&t(xt),e&&t(nt),e&&t(It),e&&t(ft),e&&t(yt),e&&t(ut),e&&t(St),e&&t(Je),e&&t(Ot),e&&t(Ue),e&&t(Ht),e&&t(ze),Zt=!1,rl()}}}function Zl(){alert("You pressed me!")}class gl extends Ct{constructor(d){super(),Mt(this,d,null,Xl,Rt,{})}}class ei extends Ct{constructor(d){super(),Mt(this,d,null,null,Rt,{})}}function ti(je){let d,c,P,E,A,w,N,L,q,I,x,b,R,k,p;return I=new Yl({}),b=new gl({}),k=new ei({}),{c(){d=a("h1"),c=s("Welcome to SvelteKit"),P=_(),E=a("p"),A=s("Visit "),w=a("a"),N=s("kit.svelte.dev"),L=s(" to read the documentation"),q=_(),gt(I.$$.fragment),x=_(),gt(b.$$.fragment),R=_(),gt(k.$$.fragment),this.h()},l(u){d=o(u,"H1",{});var h=f(d);c=r(h,"Welcome to SvelteKit"),h.forEach(t),P=m(u),E=o(u,"P",{});var D=f(E);A=r(D,"Visit "),w=o(D,"A",{href:!0});var y=f(w);N=r(y,"kit.svelte.dev"),y.forEach(t),L=r(D," to read the documentation"),D.forEach(t),q=m(u),el(I.$$.fragment,u),x=m(u),el(b.$$.fragment,u),R=m(u),el(k.$$.fragment,u),this.h()},h(){v(w,"href","https://kit.svelte.dev")},m(u,h){i(u,d,h),l(d,c),i(u,P,h),i(u,E,h),l(E,A),l(E,w),l(w,N),l(E,L),i(u,q,h),tl(I,u,h),i(u,x,h),tl(b,u,h),i(u,R,h),tl(k,u,h),p=!0},p:Fe,i(u){p||(ll(I.$$.fragment,u),ll(b.$$.fragment,u),ll(k.$$.fragment,u),p=!0)},o(u){il(I.$$.fragment,u),il(b.$$.fragment,u),il(k.$$.fragment,u),p=!1},d(u){u&&t(d),u&&t(P),u&&t(E),u&&t(q),sl(I,u),u&&t(x),sl(b,u),u&&t(R),sl(k,u)}}}class si extends Ct{constructor(d){super(),Mt(this,d,null,ti,Rt,{})}}export{si as default};
