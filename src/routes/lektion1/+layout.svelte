49bce8009ac57f8844cb12d3d3a520c77ae7b1db
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-01 22:57:46 +0100

    change folder names and add phone number thingy

--- src/routes/lektion1/+layout.svelte
+++ src/routes/lektion1/+layout.svelte
@@ -1,12 +1,19 @@
 <script>
-  let links = ["sizedropdown", "phonenumber", "movies"];
-</script>
+  import { base } from "$app/paths";
+  import { page } from "$app/stores";
+  let links = ["size-dropdown", "phone-number", "movies"];
+  //console.log("routeID: " + $page.routeId);
+  //console.log("pathname: " + $page.url.pathname);
 
-<baseURL />
+  function caps(input) {
+    input = input.replaceAll("-", " ");
+    return input[0].toUpperCase() + input.slice(1);
+  }
+</script>
 
 <nav>
   {#each links as link}
-    <a href="./{link}">{link}</a>
+    <a href="{base}/lektion1/{link}">{caps(link)}</a>
   {/each}
 </nav>
 
@@ -14,4 +21,7 @@
 
 <style>
   /* your styles go here */
+  a {
+    margin-right: 30px;
+  }
 </style>
---
+++ src/routes/lektion1/phone-number/+page.svelte
@@ -0,0 +1,66 @@
+<script>
+  // your script goes here
+  let number = null;
+  let leadingzero = 0;
+  let length;
+  $: length = Array.from(String(number)).length;
+  $: console.log(length);
+
+  $: if (number === null) {
+    leadingzero = 0;
+    console.log(leadingzero);
+  } else if (number === 0) {
+    leadingzero = 1;
+  }
+</script>
+
+<div>
+  <label for="phonenumber">Enter a phone number</label>
+  <input type="number" bind:value={number} name="" id="phonenumber" />
+</div>
+<div>
+  {#if number === null || undefined}
+    <p>Enter a phone number</p>
+  {:else if leadingzero === 1}
+    {#if length === 9}
+      <!-- content here -->
+      Your phone number is 0{Array.from(String(number))
+        .slice(0, 2)
+        .join("")}-{Array.from(String(number))
+        .slice(2, 5)
+        .join("")}-{Array.from(String(number)).slice(5, 9).join("")}
+    {:else}
+      Your phone number is 0{Array.from(String(number))
+        .slice(0, 1)
+        .join("")}-{Array.from(String(number))
+        .slice(1, 4)
+        .join("")}-{Array.from(String(number)).slice(4, 9).join("")}
+      <!-- else content here -->
+    {/if}
+  {:else if length === 10}
+    <!-- content here -->
+    <p>
+      Your phone number is {Array.from(String(number))
+        .slice(0, 3)
+        .join("")}-{Array.from(String(number))
+        .slice(3, 6)
+        .join("")}-{Array.from(String(number)).slice(6, 10).join("")}
+    </p>
+  {:else}
+    <p>
+      Your phone number is {Array.from(String(number))
+        .slice(0, 2)
+        .join("")}-{Array.from(String(number))
+        .slice(2, 5)
+        .join("")}-{Array.from(String(number)).slice(5, 10).join("")}
+    </p>
+    <!-- else content here -->
+  {/if}
+</div>
+
+<style>
+  /* your styles go here */
+  div {
+    padding: 40px;
+  }
+</style>
