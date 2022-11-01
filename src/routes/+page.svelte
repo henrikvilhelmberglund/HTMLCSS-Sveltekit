49ca02145b509729f728480eba43c4d94b9fff33
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-01 17:04:56 +0100

    move components to the subfolder page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-  import HTMLCSS_000 from "../HTMLCSS_000.svelte";
-  import HTMLCSS_000Exercise from "../HTMLCSS_000_exercise.svelte";
-  import HTMLCSS_000Exercise2 from "../HTMLCSS_000_exercise2.svelte";
+  import { base } from "$app/paths";
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
-<HTMLCSS_000 />
-<HTMLCSS_000Exercise />
-<HTMLCSS_000Exercise2 />
+
+<style>
+  /* your styles go here */
+</style>
---
+++ src/routes/lektion1/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+  // your script goes here
+  import HTMLCSS_000 from "./HTMLCSS_000.svelte";
+  import HTMLCSS_000Exercise from "./HTMLCSS_000_exercise.svelte";
+  import HTMLCSS_000Exercise2 from "./HTMLCSS_000_exercise2.svelte";
+</script>
+
+<div class="tab">
+  <a href="#page1">Page 1</a>
+  <a href="#page2">Page 2</a>
+  <a href="#page3">Page 3</a>
+
+  <div id="page1">
+    <HTMLCSS_000 />
+  </div>
+
+  <div id="page2">
+    <HTMLCSS_000Exercise />
+  </div>
+
+  <div id="page3">
+    <HTMLCSS_000Exercise2 />
+  </div>
+</div>
