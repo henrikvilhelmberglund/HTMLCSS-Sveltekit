efa785e8d667bd1ad122948e7a493439e4d6444f
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-01 23:24:39 +0100

    update size drepdown

--- src/routes/lektion1/size-dropdown/+page.svelte
+++ src/routes/lektion1/size-dropdown/+page.svelte
@@ -1,13 +1,37 @@
 <script>
   let sizes = ["XS", "S", "M", "L", "XL"];
+  let size;
 </script>
 
-<select name="size" id="size-dropdown">
-  <option value="" disabled hidden selected>Select size</option>
-  {#each sizes as size}
-    <option value={size}>{size}</option>
-  {/each}
-</select>
+<div>
+  <select bind:value={size} name="size" id="size-dropdown">
+    <option value="" disabled hidden selected>Select size</option>
+    {#each sizes as size}
+      <option value={size}>{size}</option>
+    {/each}
+  </select>
+</div>
+
+<span class="is-inline">
+  <p>You picked {size}!</p>
+  {#if size === "XS"}
+    <p>You must be tiny.</p>
+  {:else if size === "S"}
+    <p>No need to diet.</p>
+  {:else if size === "M"}
+    <p>Average size!</p>
+  {:else if size === "L"}
+    <p>Big boi!</p>
+  {:else if size === "XL"}
+    <p>You can do it!</p>
+  {/if}
+</span>
 
 <style>
+  div {
+    padding: 40px;
+  }
+  .is-inline * {
+    display: inline;
+  }
 </style>
