50fd52f6e904a57bd0c8568c3eca36ee248ed244
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-01 17:05:09 +0100

    add global navigation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  // your script goes here
+  import { base } from "$app/paths";
+</script>
+
+<nav>
+  <a href="{base}/">Home</a>
+  <a href="{base}/lektion1">Lektion 1</a>
+</nav>
+
+<slot />
