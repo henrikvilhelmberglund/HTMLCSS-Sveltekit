a1c314dbe58e2d7145665d13ac32f34361dce8fc
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-02 14:57:35 +0100

    add movies page

---
+++ src/routes/lektion1/movies/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  let thingys = ["a", "b", "c"];
+</script>
+
+<!-- 
+  Skapa en numrerad lista över namnen på dina topp 5 filmer.
+  Skapa en onumrerad lista där du skriver upp fem alternativa.
+-->
