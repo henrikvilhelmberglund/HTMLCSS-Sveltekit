be1d6ab10579267295e3fbcaeb2d4f1eb3472c23
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-02 21:44:23 +0100

    feat: :art: get article data from layout.js even in the component, use the component in the main page

--- src/routes/lektion1/articles/+page.svelte
+++ src/routes/lektion1/articles/+page.svelte
@@ -1,6 +1,8 @@
 <script>
   import { base } from "$app/paths";
   export let data;
+  import Article from "./Article.svelte";
+  let articles = Array.from(Array(3));
 </script>
 
 <!-- Huvudinnehållet på sidan ska innehålla tre nyhetsartiklar. Dessa ska innehålla en bild, en huvudrubrik, underrubrik och brödtext.
@@ -8,20 +10,32 @@
   Blir du klar: Testa använda och experimentera med CSS för att styla dessa! Se om du kan lista ut hur du t.ex kan lägga en ram runt varje artikel, färga och ändra storlek på texterna, och skapa mer mellanrum mellan element.
  -->
 
+<!-- 
+
+   <main>
+     {#each data.articles as article, i}
+     <article>
+       <a href="{base}/lektion1/articles/article{+i + 1}"
+       ><img src={article.img} alt={article.altimg} class="article-image" /></a
+       >
+       <h2>{article.title}</h2>
+       <h3>{article.subtitle}</h3>
+       <p>{article.text}</p>
+      </article>
+      {/each}
+    </main>
+  -->
+
 <main>
-  {#each data.articles as article, i}
+  {#each articles as whatever, i}
     <article>
-      <a href="{base}/lektion1/articles/article{+i + 1}"
-        ><img src={article.img} alt={article.altimg} class="article-image" /></a
-      >
-      <h2>{article.title}</h2>
-      <h3>{article.subtitle}</h3>
-      <p>{article.text}</p>
+      <Article articleNumber={i} />
     </article>
   {/each}
 </main>
 
 <style>
+  /* 
   .article-image {
     width: 20%;
   }
@@ -31,4 +45,5 @@
   h3 {
     font-style: oblique;
   }
+  */
 </style>
