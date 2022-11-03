1740ddaafd9de94f11753e08c9a6cf67d3fa1e2b
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-11-03 11:06:56 +0100

    feat: :art: add external CSS that doesn't get HMR so just use style tag in component/page

--- src/routes/lektion2/+layout.svelte
+++ src/routes/lektion2/+layout.svelte
@@ -2,6 +2,10 @@
   import { base } from "$app/paths";
 </script>
 
+<svelte:head>
+  <link rel="stylesheet" href="{base}/style.css" />
+</svelte:head>
+
 <slot />
 
 <style>
--- src/routes/lektion2/+page.svelte
+++ src/routes/lektion2/+page.svelte
@@ -6,7 +6,45 @@
   <title>Lektion 2</title>
 </svelte:head>
 
-<p>hi</p>
+<div>
+  <h1>This is a h1</h1>
+  <ul>
+    <li>thingy</li>
+    <li>thingy2</li>
+  </ul>
+  <p>hi</p>
+
+  <a href=""> <h2>Bigger text</h2></a>
+</div>
 
 <style>
+  div {
+    background-color: skyblue;
+  }
+
+  div h1 {
+    color: hsl(219, 64%, 33%);
+    font-size: 48px;
+  }
+
+  ul {
+    background-color: hsl(35, 100%, 69%);
+  }
+
+  ul li {
+    list-style: circle;
+    background-color: bisque;
+    text-align: center;
+  }
+  a {
+    color: red;
+    text-decoration: underline;
+  }
+
+  ul:hover {
+    color: rgba(0, 0, 0, 0.3);
+  }
+  h2 {
+    color: aquamarine;
+  }
 </style>
