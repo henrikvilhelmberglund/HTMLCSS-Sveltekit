f8551744579f15552941f646ad5096ed1b4755d9
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-11-03 09:28:25 +0100

    feat: wrap article content in a tag to make everything a link

--- src/routes/lektion1/articles/Article.svelte
+++ src/routes/lektion1/articles/Article.svelte
@@ -4,22 +4,23 @@
   import { page } from "$app/stores";
 </script>
 
-<article>
-  <a
-    href="{base}/lektion1/articles/article{+articleNumber + 1}"
-    target="_blank"
-    rel="noreferrer"
-    ><img
+<a
+  href="{base}/lektion1/articles/article{+articleNumber + 1}"
+  target="_blank"
+  rel="noreferrer"
+>
+  <article>
+    <img
       src={$page.data.articles[articleNumber].img}
       alt={$page.data.articles[articleNumber].altimg}
       title={$page.data.articles[articleNumber].altimg}
       class="article-image"
-    /></a
-  >
-  <h2>{$page.data.articles[articleNumber].title}</h2>
-  <h3>{$page.data.articles[articleNumber].subtitle}</h3>
-  <p>{$page.data.articles[articleNumber].text}</p>
-</article>
+    />
+    <h2>{$page.data.articles[articleNumber].title}</h2>
+    <h3>{$page.data.articles[articleNumber].subtitle}</h3>
+    <p>{$page.data.articles[articleNumber].text}</p>
+  </article>
+</a>
 
 <style>
   .article-image {
