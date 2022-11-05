a3ef6a52cf97833c06a5f6fad1121b16aa541f9a
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-05 22:47:16 +0100

    fix: :fire: remove empty page reference, add underscore to get rid of errorlens stuff

--- src/routes/lektion1/+page.svelte
+++ src/routes/lektion1/+page.svelte
@@ -2,10 +2,9 @@
   // your script goes here
   import HTMLCSS_000 from "./HTMLCSS_000.svelte";
   import HTMLCSS_000Exercise from "./HTMLCSS_000_exercise.svelte";
-  import HTMLCSS_000Exercise2 from "./HTMLCSS_000_exercise2.svelte";
   let visible = 0;
 
-  function tabClick(e, pageNum) {
+  function tabClick(_event, pageNum) {
     visible = pageNum;
     //console.log(visible);
   }
@@ -15,16 +14,13 @@
   <title>Lektion 1</title>
 </svelte:head>
 
-<button on:click={() => tabClick(event, 1)}>Page 1</button>
-<button on:click={() => tabClick(event, 2)}>Page 2</button>
-<button on:click={() => tabClick(event, 3)}>Page 3</button>
+<button on:click={(event) => tabClick(event, 1)}>Page 1</button>
+<button on:click={(event) => tabClick(event, 2)}>Page 2</button>
 
 <div class="tab">
   {#if visible === 1}
     <HTMLCSS_000 />
   {:else if visible === 2}
     <HTMLCSS_000Exercise />
-  {:else if visible === 3}
-    <HTMLCSS_000Exercise2 />
   {/if}
 </div>
