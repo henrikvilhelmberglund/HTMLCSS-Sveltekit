eb5c7d7df405255b46c7838b679fdcf2e3ebef0e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-09 21:32:04 +0100

    feat: :art: update cheat-sheet

--- src/routes/lektion3/cheat-sheet/+page.svelte
+++ src/routes/lektion3/cheat-sheet/+page.svelte
@@ -19,14 +19,20 @@
       <th>Results</th>
     </tr>
     <tr>
-      <td>Universal <span class="gray">Selector</span> </td>
-      <td>*</td>
-      <td>Select all elements </td>
-      <td
-        ><span class="hit">a</span><span class="hit">b</span><span class="hit"
-          >c</span
-        ><span class="hit">d</span></td
-      >
+      <td class="left">
+        <span class="bigger-text">Universal</span>
+        <span class="gray smaller-text">Selector</span>
+      </td>
+      <td>
+        <span class="bigger-text">*</span>
+      </td>
+      <td class="left tiny-text">Select all elements </td>
+      <td>
+        <span class="hit">a</span>
+        <span class="hit">b</span>
+        <span class="hit">c</span>
+        <span class="hit">d</span>
+      </td>
     </tr>
     <tr>
       <td colspan="4">
@@ -34,10 +40,21 @@
       </td>
     </tr>
     <tr>
-      <td>Type <span class="gray">Selector</span></td>
-      <td>a</td>
-      <td>a</td>
-      <td>a</td>
+      <td class="left">
+        <span class="bigger-text">Type</span>
+        <span class="gray smaller-text">Selector</span>
+      </td>
+      <td><span class="bigger-text">div</span></td>
+      <td class="left">
+        <div class="tiny-text">Select all elements of that type</div>
+        <div class="tiny-text">Select div elements</div>
+      </td>
+      <td>
+        <span class="not-hit">a</span>
+        <span class="hit">div</span>
+        <span class="not-hit">c</span>
+        <span class="hit">div</span>
+      </td>
     </tr>
     <tr>
       <td colspan="4">
@@ -46,10 +63,21 @@
     </tr>
 
     <tr>
-      <td>Class <span class="gray">Selector</span></td>
-      <td>.c</td>
-      <td>a</td>
-      <td>a</td>
+      <td class="left">
+        <span class="bigger-text">Class</span>
+        <span class="gray smaller-text">Selector</span>
+      </td>
+      <td><span class="bigger-text">.c</span></td>
+      <td class="left">
+        <div class="tiny-text">Select elements with that class</div>
+        <div class="gray tiny-text">Select elements with the c class</div>
+      </td>
+      <td>
+        <span class="not-hit">.a</span>
+        <span class="not-hit">.b</span>
+        <span class="hit">.c</span>
+        <span class="not-hit">.d</span>
+      </td>
     </tr>
 
     <tr>
@@ -59,10 +87,24 @@
     </tr>
 
     <tr>
-      <td>id <span class="gray">Selector</span></td>
-      <td>#i</td>
-      <td>a</td>
-      <td>a</td>
+      <td class="left">
+        <span class="bigger-text">Id</span>
+        <span class="gray smaller-text">Selector</span>
+      </td>
+      <td><span class="bigger-text">#i</span></td>
+      <td class="left">
+        <div class="tiny-text">Select elements with that id</div>
+        <div class="gray tiny-text">Select elements with the i id</div>
+        <div class="gray tiny-text">
+          *It is best practice to not use ids in CSS
+        </div>
+      </td>
+      <td>
+        <span class="not-hit">#a</span>
+        <span class="not-hit">#b</span>
+        <span class="hit">#i</span>
+        <span class="not-hit">#d</span>
+      </td>
     </tr>
   </table>
 </div>
