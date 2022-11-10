6fa8f40b84e222da157496f31d6be6bd74b0248d
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-10 22:11:32 +0100

    feat: :art: replace tables with flexboxes

--- src/routes/lektion3/cheat-sheet-svelte/+page.svelte
+++ src/routes/lektion3/cheat-sheet-svelte/+page.svelte
@@ -28,9 +28,9 @@
         ],
         [
           ["ID", "Selector"],
-          "*",
+          "#i",
           [
-            "Select all elements",
+            "Select elements with that id",
             "Select elements with the i id",
             "*It is best practice to not use ids in CSS",
           ],
@@ -65,153 +65,138 @@
     {#if table.title}
       <h2>{table.title}</h2>
       <!-- <p>{table.subtitle}</p> -->
-      <table>
+      <div class="flex-row">
+        <!-- <div class="flex-row"> -->
         {#each table.subtitle as subtitle}
-          <th>
-            {subtitle}
-          </th>
+          <div class="flex-column">
+            <div class="subtitle">
+              {subtitle}
+            </div>
+          </div>
         {/each}
-        {#each table.rows as rows}
-          <tr>
-            {#each rows as row, i}
+      </div>
+      {#each table.rows as rows, i}
+        <div class="flex-row">
+          {#each rows as row, i}
+            <!-- <div class="flex-row"> -->
+            <!-- <div class="left"> -->
+            <div class="flex-column">
               {#if i === 0}
-                <td class="left">
+                <div class="flex-child">
                   {#each row as span, i}
-                    <span class="name-{i}">{span}</span>
+                    <!-- <div> -->
+
+                    <span class="name-{i}">
+                      {span}
+                    </span>
+                    <!-- </div> -->
                   {/each}
-                </td>
+                </div>
               {:else if i === 1}
-                <td>
-                  <div class="CSS">{row}</div>
-                </td>
+                <!-- <div class="flex-child" /> -->
+                {row}
                 <!-- NOTE HTML -->
               {:else if i === 2}
-                <td>
-                  {#each row as span, i}
-                    <div class="description-{i}">{span}</div>
-                  {/each}
-                </td>
+                <!-- <div> -->
+                <!-- <div class="flex-child" /> -->
+                {#each row as span, i}
+                  <div class="description-{i}">
+                    {span}
+                  </div>
+                {/each}
+                <!-- </div> -->
                 <!-- <td> -->
-                <!-- {row} -->
                 <!-- </td> -->
               {:else if i === 3}
-                <td>
+                <!-- <div class="flex-child" /> -->
+                <!-- <div> -->
+                <div class="flex-child">
                   {#each row as span, i}
                     {#if span.toUpperCase() === span}
-                      <span class="hit">{span}</span>
+                      <span class="hit">
+                        {span}
+                      </span>
                     {:else}
-                      <span class="not-hit">{span}</span>
+                      <span class="not-hit">
+                        {span}
+                      </span>
                     {/if}
                   {/each}
-                </td>
+                </div>
+                <!-- </div> -->
               {:else}
-                <td>
-                  {row}
-                </td>
+                <!-- <div> -->
+                <!-- {row} -->
+                <!-- </div> -->
               {/if}
-            {/each}
-          </tr>
-        {/each}
-      </table>
+            </div>
+          {/each}
+        </div>
+        {#if i !== 3}
+          <div class="border" />
+        {/if}
+      {/each}
+      <!-- </div> -->
     {/if}
   {/each}
 </div>
 
-<!-- <tr>
-      <th>Name</th>
-      <th>CSS</th>
-      <th>Description</th>
-      <th>Results</th>
-    </tr>
-    <tr>
-      <td class="left">
-        <span class="bigger-text">Universal</span>
-        <span class="gray smaller-text">Selector</span>
-      </td>
-      <td>
-        <span class="bigger-text">*</span>
-      </td>
-      <td class="left tiny-text">Select all elements </td>
-      <td>
-        <span class="hit">a</span>
-        <span class="hit">b</span>
-        <span class="hit">c</span>
-        <span class="hit">d</span>
-      </td>
-    </tr>
-    <tr>
-      <td colspan="4">
-        <div class="border" />
-      </td>
-    </tr>
-    <tr>
-      <td class="left">
-        <span class="bigger-text">Type</span>
-        <span class="gray smaller-text">Selector</span>
-      </td>
-      <td><span class="bigger-text">div</span></td>
-      <td class="left">
-        <div class="tiny-text">Select all elements of that type</div>
-        <div class="tiny-text">Select div elements</div>
-      </td>
-      <td>
-        <span class="not-hit">a</span>
-        <span class="hit">div</span>
-        <span class="not-hit">c</span>
-        <span class="hit">div</span>
-      </td>
-    </tr>
-    <tr>
-      <td colspan="4">
-        <div class="border" />
-      </td>
-    </tr>
-
-    <tr>
-      <td class="left">
-        <span class="bigger-text">Class</span>
-        <span class="gray smaller-text">Selector</span>
-      </td>
-      <td><span class="bigger-text">.c</span></td>
-      <td class="left">
-        <div class="tiny-text">Select elements with that class</div>
-        <div class="gray tiny-text">Select elements with the c class</div>
-      </td>
-      <td>
-        <span class="not-hit">.a</span>
-        <span class="not-hit">.b</span>
-        <span class="hit">.c</span>
-        <span class="not-hit">.d</span>
-      </td>
-    </tr>
-
-    <tr>
-      <td colspan="4">
-        <div class="border" />
-      </td>
-    </tr>
-
-    <tr>
-      <td class="left">
-        <span class="bigger-text">Id</span>
-        <span class="gray smaller-text">Selector</span>
-      </td>
-      <td><span class="bigger-text">#i</span></td>
-      <td class="left">
-        <div class="tiny-text">Select elements with that id</div>
-        <div class="gray tiny-text">Select elements with the i id</div>
-        <div class="gray tiny-text">
-          *It is best practice to not use ids in CSS
-        </div>
-      </td>
-      <td>
-        <span class="not-hit">#a</span>
-        <span class="not-hit">#b</span>
-        <span class="hit">#i</span>
-        <span class="not-hit">#d</span>
-      </td>
-    </tr> -->
 <style>
+  .flex-column {
+    /* background-color: green; */
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    /* align-items: center; */
+    width: 300px;
+    height: 30px;
+  }
+
+  .flex-column:nth-of-type(1) {
+    /* background-color: rgb(128, 0, 113); */
+    align-items: left;
+    flex: 1;
+  }
+  .flex-column:nth-of-type(2) {
+    /* background-color: rgb(128, 0, 113); */
+    align-items: center;
+    flex: 0.7;
+  }
+  .flex-column:nth-of-type(3) {
+    /* background-color: rgb(128, 0, 113); */
+    /* align-items: center; */
+    flex: 1;
+  }
+  .flex-column:nth-of-type(4) {
+    /* background-color: rgb(128, 0, 113); */
+    align-items: center;
+    flex: 1;
+  }
+
+  .flex-row {
+    /* background-color: red; */
+    /* width: 700px; */
+    /* padding: 10px; */
+    display: flex;
+    /* flex-direction: row; */
+    /* justify-content: space-around; */
+    /* align-items: center; */
+    /* align-self: center; */
+    /* flex-wrap: wrap; */
+    /* width: 30%; */
+  }
+  .flex-row:nth-of-type(1) {
+    /* background-color: red; */
+    display: flex;
+    text-align: center;
+    /* justify-content: space-around; */
+  }
+  .flex-child {
+    /* background-color: yellow; */
+    /* padding: 10px; */
+  }
+
   h2 {
     padding: 10px;
     padding-bottom: 0px;
@@ -231,13 +216,13 @@
   }
 
   .border {
-    position: relative;
+    /* position: relative; */
     /* right: 5px; */
-    border-bottom: 2px solid #777;
+    border-bottom: 1px solid #777;
     /* width: 170%; */
     padding: 0px;
     width: 98%;
-    right: 10px;
+    /* right: 10px; */
   }
 
   table {
@@ -268,36 +253,50 @@
 
   .hit {
     background-color: hsl(208, 69%, 51%);
-    padding: 1px;
+    /* padding: 1px; */
     margin: 5px;
-    padding-left: 6px;
-    padding-right: 6px;
+    padding-left: 4px;
+    padding-right: 4px;
     border-radius: 4px;
     text-transform: lowercase;
+    font-weight: 700;
   }
 
   .not-hit {
     border: 1px solid #aaa;
     color: #aaa;
     margin: 5px;
-    padding-left: 6px;
-    padding-right: 6px;
+    padding-left: 4px;
+    padding-right: 4px;
     border-radius: 4px;
+    font-weight: 700;
+  }
+
+  .subtitle {
+    font-size: 1rem;
+    color: #aaa;
+    font-weight: 800;
+    /* padding: 3px; */
+    /* width: 100px; */
   }
 
   .name-0 {
     font-size: 1.3rem;
     padding: 3px;
+    /* width: 100px; */
   }
 
   .name-1 {
     font-size: 0.8rem;
     font-weight: 700;
     color: #777;
+    /* width: 100px; */
   }
 
   .CSS {
     font-size: 1.3rem;
+    /* width: 300px; */
+    /* width: 300px; */
   }
 
   .description-0 {
@@ -306,13 +305,13 @@
   }
 
   .description-1 {
-    font-size: 0.8rem;
+    font-size: 0.6rem;
     font-weight: 700;
     color: #777;
   }
 
   .description-2 {
-    font-size: 0.8rem;
+    font-size: 0.6rem;
     font-weight: 700;
     color: #777;
   }
