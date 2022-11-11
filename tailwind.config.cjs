cce494212425e1bf54a01d1bc135f703daf16c09
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-11 16:46:49 +0100

    feat: :sparkles: add JIT mode for tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
-
+  mode: 'jit',
+  content: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,svelte}',
+  ],
   theme: {
     extend: {},
   },
