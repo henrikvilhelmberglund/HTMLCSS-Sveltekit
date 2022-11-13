20580dcbc817dc81c5c9e82ef1048cda837c2908
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-13 16:23:38 +0100

    feat: :art: update cheat-sheet-svelte

--- src/routes/lektion3/cheat-sheet-svelte/+page.svelte
+++ src/routes/lektion3/cheat-sheet-svelte/+page.svelte
@@ -50,7 +50,7 @@
             "Select anchors that are inside a div",
           ],
           // TODO
-          ["A", "B", "C", "D"],
+          [["div"], ["A", "b"], ["A", "c", "A", "d"]],
         ],
         // prettier-ignore
         [["Direct Child","Selector"],"div > a",["Select elements that are direct children of the first element", "Select anchors that are direct children of a div"], ["div a b a c a d"],],
@@ -58,10 +58,10 @@
           ["General Sibling", "Selector"],
           "div ~ a",
           [
-            "Select elements that are siblings of the first element and come after the first element",
-            "Selects all anchors that are siblings of a div and come after the div",
+            "Select elements that are direct children of the first element",
+            "Select anchors that are direct children of a div",
           ],
-          ["a div a a"],
+          [["div"], ["A", "b"], ["a", "c", "a", "d"]],
         ],
         [
           ["Adjacent Sibling", "Selector"],
@@ -92,10 +92,115 @@
         ],
       ],
     },
-    {},
-    {},
-    {},
-    {},
+    {
+      title: "Attribute",
+      subtitle: ["Name", "CSS", "Description", "Results"],
+      rows: [
+        [
+          ["Has", "Attribute"],
+          "[a]",
+          [
+            "Select elements that have that attribute",
+            "Select elements with the a attribute",
+          ],
+          ["[A]", "[A=”1”]", "[c]", "d"],
+        ],
+        [
+          ["Exact", "Attribute"],
+          "[a=”1”]",
+          [
+            "Select elements that have that attribute with exactly that value",
+            "Select elements with the a attribute with a value of 1",
+          ],
+          ["[a]", "[A=”1”]", "[c]", "d"],
+        ],
+        [
+          ["Begins With", "Attribute"],
+          "[a^=”1”]",
+          [
+            "Select elements that have that attribute which start with that value",
+            "Select elements with the a attribute with a value that starts with 1",
+          ],
+          ["[A=”12”]", "[a=”21”]"],
+        ],
+        [
+          ["Ends With", "Attribute"],
+          "[a$=”1”]",
+          [
+            "Select elements that have that attribute which end with that value",
+            "Select elements with the a attribute with a value that ends with 1",
+          ],
+          ["[a=”12”]", "[A=”21”]"],
+        ],
+        [
+          ["Substring ", "Attribute"],
+          "[a*=”1”]",
+          [
+            "Select elements that have that attribute which contain that value anywhere",
+            "Select elements with the a attribute with a value that contains a 1",
+          ],
+          ["[A=”12”]", "[A=”21”]"],
+        ],
+      ],
+    },
+    {
+      title: "Pseudo Element",
+      subtitle: ["Name", "CSS", "Description", "Results"],
+      rows: [
+        [
+          ["Before", "Selector"],
+          "div::before",
+          [
+            "Creates an empty element directly before the children of selected element",
+          ],
+          ["div BEFORE c after"],
+        ],
+        [
+          ["After", "Selector"],
+          "div::after",
+          [
+            "Creates an empty element directly after the children of selected element",
+          ],
+          ["div before c AFTER"],
+        ],
+      ],
+    },
+    {
+      title: "Pseudo Class (State)",
+      subtitle: ["Name", "CSS", "Description"],
+      rows: [
+        [
+          ["Hover", "Selector"],
+          "button:hover",
+          [
+            "Select elements that are hovered by the mouse",
+            "Select buttons that are being hovered",
+          ],
+        ],
+        [
+          ["After", "Selector"],
+          "div::after",
+          [
+            "Creates an empty element directly after the children of selected element",
+          ],
+          ["div before c AFTER"],
+        ],
+      ],
+    },
+    {
+      title: "Pseudo Class (Position/Other)",
+      subtitle: ["Name", "CSS", "Description", "Results"],
+      rows: [
+        [
+          ["Hover", "Selector"],
+          "button:hover",
+          [
+            "Select elements that are hovered by the mouse",
+            "Select buttons that are being hovered",
+          ],
+        ],
+      ],
+    },
   ];
 </script>
 
@@ -138,7 +243,8 @@
           </div>
         {/each}
       </div>
-      {#each table.rows as rows, i}
+      <!-- {table.rows.length} -->
+      {#each table.rows as rows, rl}
         <div class="flex-row-{y}">
           {#each rows as row, i}
             <!-- <div class="flex-row"> -->
@@ -173,9 +279,48 @@
               {:else if i === 3}
                 <!-- <div class="flex-child" /> -->
                 <!-- <div> -->
-                <div class="flex-child">
-                  {#each row as span, i}
-                    {#if span.toUpperCase() === span}
+
+                <div class="nice-div">
+                  {#each row as span, r}
+                    <!-- {console.log(span)}
+                    {console.log(typeof span)} -->
+                    {#if typeof span === "object"}
+                      <div class="flex-row-with-children">
+                        {#each span as element, s}
+                          {#if typeof element === "object"}
+                            <div class="flex-row-with-children">
+                              {#each element as inner, e}
+                                {#if typeof inner === "object"}
+                                  console.log("what")
+                                {:else if inner.toUpperCase() === inner}
+                                  <span class="hit">
+                                    {inner}
+                                  </span>
+                                {:else}
+                                  <span class="not-hit">
+                                    {inner}
+                                  </span>
+                                {/if}
+                              {/each}
+                            </div>
+                          {:else if element.toUpperCase() === element}
+                            <div class="tiny-border-{r}">
+                              <span class="hit">
+                                {element}
+                              </span>
+                              <div class="tiny-line-{r}" />
+                            </div>
+                          {:else}
+                            <div class="tiny-border-{r}">
+                              <span class="not-hit">
+                                {element}
+                              </span>
+                              <div class="tiny-line-{r}" />
+                            </div>
+                          {/if}
+                        {/each}
+                      </div>
+                    {:else if span.toUpperCase() === span}
                       <span class="hit">
                         {span}
                       </span>
@@ -186,16 +331,11 @@
                     {/if}
                   {/each}
                 </div>
-                <!-- </div> -->
-              {:else}
-                <!-- <div> -->
-                <!-- {row} -->
-                <!-- </div> -->
               {/if}
             </div>
           {/each}
         </div>
-        {#if i !== 3}
+        {#if rl !== table.rows.length - 1}
           <div class="border" />
         {/if}
       {/each}
@@ -262,6 +402,87 @@
     /* flex-wrap: wrap; */
     /* width: 30%; */
   }
+  .flex-row-2 {
+    /* background-color: red; */
+    /* width: 700px; */
+    /* padding: 10px; */
+    display: flex;
+    height: 100px;
+    /* flex-direction: row; */
+    /* justify-content: space-around; */
+    align-items: center;
+    /* align-self: center; */
+    /* flex-wrap: wrap; */
+    /* width: 30%; */
+  }
+  .flex-row-3 {
+    /* background-color: red; */
+    /* width: 700px; */
+    /* padding: 10px; */
+    display: flex;
+    height: 100px;
+    /* flex-direction: row; */
+    /* justify-content: space-around; */
+    align-items: center;
+    /* align-self: center; */
+    /* flex-wrap: wrap; */
+    /* width: 30%; */
+  }
+  .flex-row-4 {
+    /* background-color: red; */
+    /* width: 700px; */
+    /* padding: 10px; */
+    display: flex;
+    height: 100px;
+    /* flex-direction: row; */
+    /* justify-content: space-around; */
+    align-items: center;
+    /* align-self: center; */
+    /* flex-wrap: wrap; */
+    /* width: 30%; */
+  }
+  .flex-row-5 {
+    /* background-color: red; */
+    /* width: 700px; */
+    /* padding: 10px; */
+    display: flex;
+    height: 100px;
+    /* flex-direction: row; */
+    /* justify-content: space-around; */
+    align-items: center;
+    /* align-self: center; */
+    /* flex-wrap: wrap; */
+    /* width: 30%; */
+  }
+  .flex-row-6 {
+    /* background-color: red; */
+    /* width: 700px; */
+    /* padding: 10px; */
+    display: flex;
+    height: 100px;
+    /* flex-direction: row; */
+    /* justify-content: space-around; */
+    align-items: center;
+    /* align-self: center; */
+    /* flex-wrap: wrap; */
+    /* width: 30%; */
+  }
+
+  .flex-row-with-children {
+    /* background-color: red; */
+    /* width: 700px; */
+    /* padding: 10px; */
+    display: flex;
+    /* height: 100px; */
+    /* flex-direction: row; */
+    justify-content: center;
+    justify-content: space-around;
+    align-items: center;
+    /* align-self: center; */
+    /* flex-wrap: wrap; */
+    /* width: 30%; */
+  }
+
   .flex-row:nth-of-type(1) {
     /* background-color: red; */
     display: flex;
@@ -270,6 +491,20 @@
   }
   .flex-child {
     /* background-color: yellow; */
+    /* padding: 10px; */
+  }
+  .flex-child-results {
+    /* background-color: yellow; */
+    /* padding: 10px; */
+  }
+
+  .centered-div {
+    /* background-color: yellow; */
+    justify-content: center;
+    /* justify-content: space-around; */
+    align-items: center;
+    display: flex;
+    flex-direction: column;
     /* padding: 10px; */
   }
 
@@ -327,6 +562,51 @@
     width: 98%;
     /* right: 10px; */
   }
+  .tiny-border-0 {
+    /* display: flex; */
+    position: relative;
+    /* right: 4px; */
+    border-bottom: 2px solid #777;
+    /* margin-left: 5px; */
+    width: 60%;
+    /* padding: 0px; */
+    /* width: 98%; */
+    /* right: 10px; */
+    align-items: center;
+    justify-content: center;
+    display: flex;
+  }
+  .tiny-border-1 {
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    position: relative;
+    display: inline;
+    /* right: 5px; */
+    border-bottom: 2px solid #777;
+    margin: 3px;
+    width: 30%;
+    /* padding: 0px; */
+    /* width: 98%; */
+    /* right: 10px; */
+  }
+
+  .tiny-line-1 {
+    position: relative;
+    right: 15px;
+    top: -5px;
+    display: inline;
+    border-right: 1px solid #777;
+    height: 15px;
+  }
+  .tiny-line-2 {
+    position: relative;
+    right: 15px;
+    top: -5px;
+    display: inline;
+    border-right: 1px solid #777;
+    height: 5px;
+  }
 
   .hit {
     background-color: hsl(208, 69%, 51%);
@@ -347,6 +627,7 @@
     padding-right: 4px;
     border-radius: 4px;
     font-weight: 700;
+    z-index: 5;
   }
 
   .subtitle {
