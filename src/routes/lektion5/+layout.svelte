89baed9ba6771d0011514abf9cee986e337810c4
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-16 10:38:27 +0100

    style: :art: fix prettier jank

--- src/routes/lektion5/+layout.svelte
+++ src/routes/lektion5/+layout.svelte
@@ -5,10 +5,11 @@
 <nav>
   <a href="{base}/">Home</a>
   <a href="{base}/lektion5/flexbox-more">Flexbox-more</a>
-  <a href="{base}/lektion5/flexbox-more-mobile-first"
-    >Flexbox-more-mobile-first</a
-  >
+  <a href="{base}/lektion5/flexbox-more-mobile-first">
+    Flexbox-more-mobile-first
+  </a>
   <a href="{base}/lektion5/exercise">Exercise</a>
+  <a href="{base}/lektion5/first-exercise-remake">Remake</a>
 </nav>
 
 <slot />
