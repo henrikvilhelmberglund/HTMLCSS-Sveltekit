17e4c00c30c7145baad7595627a27bb38687ae6f
Henrik Berglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-11-17 11:01:09 +0100

    Updates

---
+++ _app/immutable/chunks/1-d883ef1f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_error.svelte-f7d90b06.js";export{t as component};
--- _app/immutable/chunks/29-c9c6e5e1.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/lektion5/exercise/_page.svelte-d8f1e9a4.js";export{t as component};
