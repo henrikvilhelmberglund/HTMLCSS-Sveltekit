e48a4bccf06be100fbf906b067af4bd0e929ddf0
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-18 11:41:55 +0100

    feat: :sparkles: add Threlte to layout nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,6 +20,7 @@
   <a href="{base}/lektion4">Lektion 4</a>
   <a href="{base}/lektion5">Lektion 5</a>
   <a href="{base}/lektion6">Lektion 6</a>
+  <a href="{base}/threlte">Threlte test</a>
 </nav>
 
 <slot />
