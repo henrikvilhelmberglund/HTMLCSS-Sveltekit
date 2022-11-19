79b7fe362b6fb06361f4d09f0da662459434727c
henrikvilhelmberglund <110549389+henrikvilhelmberglund@users.noreply.github.com>
2022-11-19 00:05:23 +0000

    Deploying to gh-pages from @ henrikvilhelmberglund/HTMLCSS-Sveltekit@206afeec22df948502ee8f81c14b4364c920b77c 🚀

--- _app/immutable/chunks/1-c0d40878.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_error.svelte-24a74878.js";export{t as component};
--- _app/immutable/chunks/29-72e93984.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/lektion5/exercise/_page.svelte-eefa7a38.js";export{t as component};
