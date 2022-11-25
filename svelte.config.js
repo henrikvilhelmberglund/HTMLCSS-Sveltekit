79a266e2012488b4bbe350f13fff8c565e6745fc
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-25 16:12:15 +0100

    feat: :sparkles: add mdsvex through npx svelte-add@latest mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,5 @@
+import { mdsvex } from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
 import preprocess from "svelte-preprocess";
 //import adapter from '@sveltejs/adapter-auto';
 import adapter from "@sveltejs/adapter-static";
@@ -8,6 +10,8 @@ const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  extensions: [".svelte", ...mdsvexConfig.extensions],
+
   kit: {
     adapter: adapter({
       // default options are shown. On some platforms
@@ -32,6 +36,7 @@ const config = {
       postcss: true,
     }),
     seqPreprocessor([preprocess(), preprocessThrelte()]),
+    mdsvex(mdsvexConfig),
   ],
 };
 
