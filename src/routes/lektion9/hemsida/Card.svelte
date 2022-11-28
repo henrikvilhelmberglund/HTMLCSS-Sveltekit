9470e4d5690ab128a208bb27fe53af034f148b39
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-28 21:56:49 +0100

    feat: :sparkles: add new features to card component

--- src/routes/lektion9/hemsida/Card.svelte
+++ src/routes/lektion9/hemsida/Card.svelte
@@ -1,9 +1,29 @@
 <script>
-  export let src
+  import { base } from "$app/paths";
+  import PDuckName from "./PDuckName.svelte";
+  import PDuckDesc from "./PDuckDesc.svelte";
+  export let duck;
+  let { type, src, alt, desc, price } = duck;
 </script>
 
-<img {src} alt="" srcset="" class="w-auto h-40 p-2 bg-emerald-700 border-2 border-solid border-white">
+<div class="flex flex-col text-center">
+  <img
+    {src}
+    {alt}
+    srcset=""
+    class="h-64 border-2 border-solid transition-all border-yellow-300 hover:h-80" />
+  <div class="flex flex-col justify-center align-center h-20">
+    <PDuckName>
+      {type}
+    </PDuckName>
+    <PDuckName newClass="h-1 text-1xl font-duckName">
+      {price}sek
+    </PDuckName>
+  </div>
+  <PDuckDesc>
+    {desc}
+  </PDuckDesc>
+</div>
 
 <style>
-  
 </style>
