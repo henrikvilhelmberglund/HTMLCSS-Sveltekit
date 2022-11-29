a458e066191fe611d5128ca6e1db5aa8b251b9b4
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-11-29 21:19:58 +0100

    fix: :bug: add default prop value which is undefined

--- src/routes/lektion9/hemsida/PDuckName.svelte
+++ src/routes/lektion9/hemsida/PDuckName.svelte
@@ -1,16 +1,17 @@
 <script>
   import "@fontsource/elsie";
-  export let newClass;
+  export let newClass = undefined;
 </script>
 
 {#if newClass}
-<p class={newClass}>
-  <slot />
-</p>
-{:else}  
-<p class="text-4xl font-duckName m-0  transition-all group-hover:underline group-hover:decoration-emerald-400 group-hover:decoration-4 group-hover:underline-offset-4 underline decoration-emerald-400/0 decoration-4 underline-offset-4 ">
-  <slot />
-</p>
+  <p class={newClass}>
+    <slot />
+  </p>
+{:else}
+  <p
+    class="text-4xl font-duckName m-0  transition-all group-hover:underline group-hover:decoration-emerald-400 group-hover:decoration-4 group-hover:underline-offset-4 underline decoration-emerald-400/0 decoration-4 underline-offset-4 ">
+    <slot />
+  </p>
 {/if}
 
 <style>
